23d7bf5b42e8a23fc161d6d98372f2629bb3edd9
BuildTools <Bayman510@gmail.com>
2020-09-14 21:42:43 -0500

    fix path

--- ProxyMaker.ps1
+++ ProxyMaker.ps1
@@ -1,6 +1,6 @@
 mkdir proxy; dir *.* | foreach-object { $newname = $_.Name.Remove($_.Name.Length - $_.Extension.Length) + "_proxy.mov"; `
 
-C:\Users\bayma\Documents\ffmpeg-20181027-59a35fe-win64-static\bin\ffmpeg.exe `
+.\bin\ffmpeg.exe `
 -hwaccel auto `
 -i "$_" `
 -acodec copy `
